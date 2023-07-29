97866d66bbd15260bfcbdb70f3b819c59127c288
peogeka <peo.geka@gmail.com>
2023-07-29 08:58:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ const loader = document.querySelector(".loader");
 const error = document.querySelector(".error");
 const catInfo = document.querySelector(".cat-info");
 
-
 function populateBreedSelect(breeds) {
-  breeds.forEach(breed => {
+  breeds.forEach((breed) => {
     const option = document.createElement("option");
     option.value = breed.id;
     option.textContent = breed.name;
@@ -15,48 +14,56 @@ function populateBreedSelect(breeds) {
   });
 }
 
-
 function toggleLoader(show) {
   loader.style.display = show ? "block" : "none";
   breedSelect.style.display = show ? "none" : "block";
 }
 
-
 function showCatInfo(catData) {
+  if (catData.length === 0) {
+    showError();
+    return;
+  }
+
   const [cat] = catData;
   const { url, breeds } = cat;
-  const { name, description, temperament } = breeds[0];
 
   const imgElement = document.createElement("img");
   imgElement.src = url;
-  imgElement.alt = "Cat Image";
-
-  const nameElement = document.createElement("h2");
-  nameElement.textContent = name;
-
-  const descriptionElement = document.createElement("p");
-  descriptionElement.textContent = description;
-
-  const temperamentElement = document.createElement("p");
-  temperamentElement.textContent = temperament;
+  imgElement.alt = "cat image";
 
   catInfo.innerHTML = "";
+
   catInfo.appendChild(imgElement);
-  catInfo.appendChild(nameElement);
-  catInfo.appendChild(descriptionElement);
-  catInfo.appendChild(temperamentElement);
+
+  if (breeds.length > 0) {
+    const { name, description, temperament } = breeds[0];
+
+    const nameElement = document.createElement("h2");
+    nameElement.textContent = name;
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = description;
+
+    const temperamentElement = document.createElement("p");
+    temperamentElement.textContent = temperament;
+
+    catInfo.appendChild(nameElement);
+    catInfo.appendChild(descriptionElement);
+    catInfo.appendChild(temperamentElement);
+  }
+
   catInfo.style.display = "block";
 }
-
 
 function showError() {
   error.style.display = "block";
 }
 
-
 toggleLoader(true);
+
 fetchBreeds()
-  .then(breeds => {
+  .then((breeds) => {
     populateBreedSelect(breeds);
     toggleLoader(false);
   })
@@ -65,12 +72,11 @@ fetchBreeds()
     showError();
   });
 
-
 breedSelect.addEventListener("change", () => {
   const selectedBreedId = breedSelect.value;
   toggleLoader(true);
   fetchCatByBreed(selectedBreedId)
-    .then(catData => {
+    .then((catData) => {
       toggleLoader(false);
       showCatInfo(catData);
     })
